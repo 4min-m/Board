7ff72ba2431d74141cdc1329af6ca55895fd3b1b
mzamanzadeh <mohammad.zamanzadeh@gmail.com>
2017-11-05 15:12:24 +0330

    set Title command

--- src/NewAdSetTitle.java
+++ src/NewAdSetTitle.java
@@ -1,3 +1,4 @@
+import org.json.JSONObject;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
@@ -5,6 +6,9 @@ import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.AbsSender;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +21,20 @@ public class NewAdSetTitle implements baseState {
     public void excecute(AbsSender absSender, SendMessage message, Update update)
     {
         MySQLAccess mySQLAccess = new MySQLAccess();
+
+        ResultSet cat_id = mySQLAccess.query("SELECT id from categories where name='"+update.getMessage().getText()+"'");
+
+        try {
+            if(!cat_id.next()) {
+                message.setText("دسته نامعتبر لطفا مجدد انتخاب کنید:"
+                        + "\r\n"
+                );
+            }else{
+
+        JSONObject jsonData = mySQLAccess.getDataOfState(update.getMessage().getChatId());
+        jsonData.put("category_id",cat_id.getInt("id"));
+
+        mySQLAccess.setDataOfState(jsonData,update.getMessage().getChatId());
 
         mySQLAccess.setState(STATE.NEW_AD_SET_TITLE,update.getMessage().getChatId());
         message.setText("لطفا عنوان آگهی را وارد کنید:"
@@ -39,7 +57,10 @@ public class NewAdSetTitle implements baseState {
 
         replyKeyboardMarkup.setKeyboard(keyboard);
         message.setReplyMarkup(replyKeyboardMarkup);
-
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         try {
             absSender.sendMessage(message); // Call method to send the message
         } catch (TelegramApiException e) {
@@ -49,7 +70,7 @@ public class NewAdSetTitle implements baseState {
 
     @Override
     public boolean isValid(Update update, int currentState) {
-        return update.getMessage().getText().equals("افزودن آگهی");
+        return currentState==STATE.NEW_AD_SET_TITLE;
     }
 
 }
